f79b11d1dd8fd38d0bcdca3414674c10c5156738
robert <robert@sqrtroot.com>
2021-02-07 18:36:01 +0100

    both generic hid and kbd/mouse hid

--- src/hid_callbacks.cpp
+++ src/hid_callbacks.cpp
@@ -3,16 +3,36 @@
 #include <algorithm>
 #include <charconv>
 
-extern "C" uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
+extern "C" uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
   return 0;
 }
 
-extern "C" void tud_hid_set_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {
-  uint32_t new_blinking_speed;
-  auto     convResult = std::from_chars((const char *) buffer, (const char *) buffer + bufsize, new_blinking_speed);
-  if(convResult.ec == std::errc() /*no error*/) {
-    const auto new_pwm_value = std::min((uint32_t) 100, new_blinking_speed);
-    System::getSystem().voltmeter.setLevel(new_pwm_value);
-    System::getSystem().blink_interval_ms = new_blinking_speed;
+extern "C" void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {
+  if(itf == 0) {
+    uint32_t new_blinking_speed;
+    auto     convResult = std::from_chars((const char *) buffer, (const char *) buffer + bufsize, new_blinking_speed);
+    if(convResult.ec == std::errc() /*no error*/) {
+      const auto new_pwm_value = std::min((uint32_t) 100, new_blinking_speed);
+      System::getSystem().voltmeter.setLevel(new_pwm_value);
+      System::getSystem().blink_interval_ms = new_blinking_speed;
+    }
+  } else if(itf == 1) {
+    if(tud_hid_n_ready(1) && false) {
+      // use to avoid send multiple consecutive zero report for keyboard
+      static bool has_key = false;
+
+      static bool toggle = false;
+      if(toggle = !toggle) {
+        uint8_t keycode[6] = {0};
+        keycode[0]         = HID_KEY_A;
+        tud_hid_n_keyboard_report(1, 1, 0, keycode);
+
+        has_key = true;
+      } else {
+        // send empty key report if previously has key pressed
+        if(has_key) tud_hid_n_keyboard_report(1, 1, 0, NULL);
+        has_key = false;
+      }
+    }
   }
 }
--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -66,33 +66,45 @@ uint8_t const *tud_descriptor_device_cb(void) {
 // HID Report Descriptor
 //--------------------------------------------------------------------+
 
-uint8_t const desc_hid_report[] = {
+uint8_t const desc_hid_io_report[] = {
   TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_BUFSIZE),    // generic hid for leds/meters
 };
+
+uint8_t const desc_hid_kbm_report[] = {TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(1)), TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(2))};
 
 // Invoked when received GET HID REPORT DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
-uint8_t const *tud_hid_descriptor_report_cb(void) {
-  return desc_hid_report;
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf) {
+  if(itf == 0) {
+    return desc_hid_io_report;
+  } else if(itf == 1) {
+    return desc_hid_kbm_report;
+  }
+
+  return NULL;
 }
 
 //--------------------------------------------------------------------+
 // Configuration Descriptor
 //--------------------------------------------------------------------+
 
-enum { ITF_NUM_HID, ITF_NUM_TOTAL };
+enum { ITF_NUM_HID_IO, ITF_NUM_HID_KBM, ITF_NUM_TOTAL };
 
-#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_INOUT_DESC_LEN)
+#define CONFIG_TOTAL_LEN \
+  (TUD_CONFIG_DESC_LEN + TUD_HID_INOUT_DESC_LEN + TUD_HID_DESC_LEN)
 
-#define EPNUM_HID 0x01
+#define EPNUM_HID_IO_I 0x1
+#define EPNUM_HID_IO_O 0x81
+#define EPNUM_HID_KBM  0x82
 
 uint8_t const desc_configuration[] = {
   // Config number, interface count, string index, total length, attribute, power in mA
   TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
   // Interface number, string index, protocol, report descriptor len, EP In & Out address, size & polling interval
-  TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID, 0, HID_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_HID, 0x80 | EPNUM_HID, CFG_TUD_HID_BUFSIZE, 10)};
+  TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID_IO, 0, HID_PROTOCOL_NONE, sizeof(desc_hid_io_report), EPNUM_HID_IO_I, EPNUM_HID_IO_O, CFG_TUD_HID_BUFSIZE, 10),
+  TUD_HID_DESCRIPTOR(ITF_NUM_HID_KBM, 0, HID_PROTOCOL_NONE, sizeof(desc_hid_kbm_report), EPNUM_HID_KBM, CFG_TUD_HID_EP_BUFSIZE, 10)};
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
