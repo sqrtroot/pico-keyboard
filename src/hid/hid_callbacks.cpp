3665b4efcdfda7d98aa8495d780d6b162cde8c47
robert <robert@sqrtroot.com>
2021-02-13 22:41:31 +0100

    Some cleanup

--- src/hid/hid_callbacks.cpp
+++ src/hid/hid_callbacks.cpp
@@ -16,6 +16,11 @@ extern "C" uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf) {
   return NULL;
 }
 
+extern "C" bool tud_hid_set_idle_cb(uint8_t itf, uint8_t idle_rate){
+  printf("Idle rate for interface %d is %d", itf, idle_rate);
+  return true;
+};
+
 extern "C" uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
   printf("tud_hid_get_report_cb\n");
   return 0;
@@ -53,7 +58,3 @@ void hid_io_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t c
   }
 }
 
-extern "C" bool tud_hid_set_idle_cb(uint8_t itf, uint8_t idle_rate){
-  printf("Idle rate for interface %d is %d", itf, idle_rate);
-  return true;
-};
